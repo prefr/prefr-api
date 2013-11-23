66e47061d93a6bcf36de9138a1e883b5db88ae6d
Reimerei <git@bjrm.de>
2013-11-23 18:46:32 +0100

    Hello world from api, added routes for frontend

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,15 @@
 package controllers
 
-import play.api._
 import play.api.mvc._
 
 object Application extends Controller {
 
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+  def assets(path: String, file: String, foo: String) =
+    controllers.Assets.at(path, file)
+
+  def index() = Action {
+    request =>
+      Ok(views.html.index())
   }
 
 }
