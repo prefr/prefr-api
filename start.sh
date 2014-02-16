bcb4d71492ea3622e84425ee1708abf2bd6cc512
Reimerei <git@bjrm.de>
2014-02-16 15:44:32 +0100

    disable colors

--- start.sh
+++ start.sh
@@ -3,4 +3,6 @@
 # get appname
 appName=$(ls | grep -v ".bat")
 
-nohup bash -c "./target/universal/stage/bin/$appName" &
+echo "Starting app: " $appName
+
+nohup bash -c "./target/universal/stage/bin/$appName -Dsbt.log.noformat=true" &> /dev/null & 
