97450f03dc93134007ede180404bee06525e4b79
reimerei <github@bjrm.de>
2014-04-20 11:22:47 +0200

    formatted code

--- app/controllers/BallotController.scala
+++ app/controllers/BallotController.scala
@@ -1,11 +1,11 @@
 package controllers
 
-import play.api.mvc.{Action, Controller}
+import play.api.mvc.{ Action, Controller }
 import play.modules.reactivemongo.MongoController
-import play.api.libs.json.{Json, JsError}
-import model.{BallotBoxUpdate, Paper, BallotBox}
+import play.api.libs.json.{ Json, JsError }
+import model.{ BallotBoxUpdate, Paper, BallotBox }
 
-import scala.concurrent.{Future, ExecutionContext}
+import scala.concurrent.{ Future, ExecutionContext }
 import ExecutionContext.Implicits.global
 import play.api.Logger
 
@@ -17,21 +17,23 @@ import play.api.Logger
 object BallotController extends Controller with MongoController {
 
   def createBallotBox = Action(parse.tolerantJson(512 * 1024)) {
-    request => {
-      request.body.validate[BallotBox](BallotBox.inputReads).map {
-        ballotBox => {
-          BallotBox.col.insert(ballotBox)
-          Logger.debug("Created Ballot: " + ballotBox.toJson)
-          Ok(ballotBox.toJson ++ Json.obj("adminSecret" -> ballotBox.adminSecret))
-        }
-      }.recoverTotal(e => BadRequest(JsError.toFlatJson(e)))
-    }
+    request =>
+      {
+        request.body.validate[BallotBox](BallotBox.inputReads).map {
+          ballotBox =>
+            {
+              BallotBox.col.insert(ballotBox)
+              Logger.debug("Created Ballot: " + ballotBox.toJson)
+              Ok(ballotBox.toJson ++ Json.obj("adminSecret" -> ballotBox.adminSecret))
+            }
+        }.recoverTotal(e => BadRequest(JsError.toFlatJson(e)))
+      }
   }
 
   def getBalloxBox(id: String) = Action.async {
     Logger.debug("Get Ballot: " + id)
     BallotBox.col.find(Json.obj("id" -> id)).one[BallotBox].map {
-      case None => NotFound
+      case None    => NotFound
       case Some(b) => Ok(b.toJson)
     }
   }
@@ -39,29 +41,30 @@ object BallotController extends Controller with MongoController {
   def addVote(id: String) = Action.async(parse.tolerantJson) {
     request =>
       request.body.validate[Paper](Paper.inputReads).map {
-        paper => {
-          Logger.debug("Add vote to id: " + id + " pater: " + paper)
+        paper =>
+          {
+            Logger.debug("Add vote to id: " + id + " pater: " + paper)
 
-          // check if vote is valid
-          paper.check match {
-            case false => Future(BadRequest("Invalid options"))
-            case true =>
+            // check if vote is valid
+            paper.check match {
+              case false => Future(BadRequest("Invalid options"))
+              case true =>
 
-              // Add paper to ballot
-              val query = Json.obj("id" -> id)
-              val set = Json.obj("$push" -> Json.obj("papers" -> paper))
+                // Add paper to ballot
+                val query = Json.obj("id" -> id)
+                val set = Json.obj("$push" -> Json.obj("papers" -> paper))
 
-              BallotBox.col.update(query, set).map {
-                lastError =>
-                  if (lastError.updatedExisting) {
-                    Ok("vote added")
-                  }
-                  else {
-                    NotFound("ballot not found")
-                  }
-              }
+                BallotBox.col.update(query, set).map {
+                  lastError =>
+                    if (lastError.updatedExisting) {
+                      Ok("vote added")
+                    }
+                    else {
+                      NotFound("ballot not found")
+                    }
+                }
+            }
           }
-        }
       }.recoverTotal(e => Future(BadRequest(JsError.toFlatJson(e))))
 
   }
@@ -73,7 +76,7 @@ object BallotController extends Controller with MongoController {
         case Some(bb) =>
           request.body.validate[BallotBoxUpdate].map {
             update =>
-            // check secret
+              // check secret
               update.adminSecret.equals(bb.adminSecret) match {
                 case false => Unauthorized("wrong admin secret")
                 case true =>
--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# get appname
-appName=$(ls ./target/universal/stage/bin/ | grep -v "[.bat|.log]")
-
-echo "Starting app: " $appName
-
-nohup bash -c "./target/universal/stage/bin/$appName -mem 200 -Dhttp.port=9001" &
