0b6a1cc6ac9bfb9d5e066a47b902e6c83b5c9d2d
Björn Reimer <git@bjrm.de>
2015-06-23 16:51:42 +0200

    bugfix

--- app/Global.scala
+++ app/Global.scala
@@ -9,22 +9,17 @@ import play.api.mvc.{EssentialAction, WithFilters, EssentialFilter}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import play.api.http.HeaderNames._
-
+import play.api.Play.current
 
 object AccessControllFilter extends EssentialFilter {
   // wrap action to modify the headers of every request
   def apply(action: EssentialAction): EssentialAction = EssentialAction {
     request =>
-      val accessControllEnabled = Play.configuration.getString("headers.accessControl.enable")
-      accessControllEnabled match {
-        case Some("true") =>
           action.apply(request).map(_.withHeaders(
             ACCESS_CONTROL_ALLOW_METHODS -> "GET, POST, DELETE, PUT, OPTIONS",
             ACCESS_CONTROL_ALLOW_ORIGIN -> "*")//,
 //            ACCESS_CONTROL_ALLOW_HEADERS -> "Authorization, Content-type, X-File-Name, X-Max-Chunks, X-File-Size, X-File-Type, X-Index, X-TwoFactorToken")
           )
-        case _ => action.apply(request)
-      }
   }
 }
 
